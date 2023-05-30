791e521b67cce23fd7bc9b098fb5075f4ed14659
Camila Mercadal <camila@MacBook-Pro-de-Camila.local>
2023-05-30 14:37:07 -0400

    Initial commit

---
+++ src/main/java/cl/camila/ApiKey.java
@@ -0,0 +1,6 @@
+package cl.camila;
+
+public class ApiKey {
+
+    public static String API_KEY= "ca55503c2d8943f68c4275145b67b096";
+}
---
+++ src/main/java/cl/camila/RecipeService.java
@@ -0,0 +1,50 @@
+package cl.camila;
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.HttpClientBuilder;
+import org.apache.http.util.EntityUtils;
+import org.json.JSONArray;
+import org.json.JSONObject;
+import cl.camila.ApiKey;
+
+public class RecipeService {
+
+    static String apiKey = ApiKey.API_KEY;
+
+    public static String getRecipesByIngredients(String ingredients) {
+        try {
+            HttpClient httpClient = HttpClientBuilder.create().build();
+            String url = "https://api.spoonacular.com/recipes/findByIngredients?apiKey=" + apiKey + "&ingredients=" + ingredients;
+            HttpGet request = new HttpGet(url);
+            HttpResponse response = httpClient.execute(request);
+            HttpEntity entity = response.getEntity();
+
+            if (entity != null) {
+                String result = EntityUtils.toString(entity);
+                return result;
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return null;
+    }
+
+    public static void main(String[] args) {
+        String ingredients = "apple,onion";
+        String result = getRecipesByIngredients(ingredients);
+        if (result != null) {
+            JSONArray recipes = new JSONArray(result);
+            for (int i = 0; i < recipes.length(); i++) {
+                JSONObject recipe = recipes.getJSONObject(i);
+                String title = recipe.getString("title");
+                int id = recipe.getInt("id");
+                System.out.println("Title: " + title);
+                System.out.println("ID: " + id);
+                System.out.println("------------");
+            }
+        }
+    }
+}
